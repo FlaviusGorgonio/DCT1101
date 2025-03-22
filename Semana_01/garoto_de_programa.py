466e6d211400b99049c7a9122fbdca6fe9c73ffc
Flavius Gorgônio <flgorgonio@gmail.com>
2025-03-21 21:43:14 -0300

    feat(semana_01): adiciona exemplo "Garoto de programa"

---
+++ Semana_01/garoto_de_programa.py
@@ -0,0 +1,11 @@
+###############################################
+########  Primeiro Programa em Python  ########
+###############################################
+
+print("Olá, pessoal de casa...")
+print()
+print("Estou muito feliz")
+print("Hoje consegui escrever meu primeiro programa")
+print("Em breve, estarei realizando o meu sonho")
+print("Me tornar um@ garot@ de programa!")
+print("Até logo!")

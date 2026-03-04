0f51b356636033d2bd370e3518704ccbc0230c13
Prof. Flavius Gorgônio <108832782+FlaviusGorgonio@users.noreply.github.com>
2026-03-04 18:28:00 -0300

    feat(semana_01): adiciona exemplo "Cálculo da média"

---
+++ Semana_01/calculo_da_media.py
@@ -0,0 +1,14 @@
+###############################################
+########   Programa Cálculo da Média   ########
+###############################################
+
+print("Cálculo da média")
+nota1 = input("Digite a 1a. nota: ")
+nota1 = float(nota1)
+nota2 = input("Digite a 2a. nota: ")
+nota2 = float(nota2)
+nota3 = input("Digite a 3a. nota: ")
+nota3 = float(nota3)
+soma = nota1 + nota2 + nota3
+media = soma / 3
+print("Sua média foi:", media)
